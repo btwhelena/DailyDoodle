f5bfe4f049ef6945f175f6554b71b1ef5f3c987c
Helena <helenaoliveira@alu.ufc.br>
2023-02-13 11:05:14 -0300

    Initial Commit

---
+++ DailyDoodle/DailyDoodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDoodleApp.swift
+//  DailyDoodle
+//
+//  Created by Helena Oliveira on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyDoodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
