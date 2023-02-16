dc651e852b7405158aabae2538d5ed7e0ae21da9
Lais Godinho <47223072+laisbastosbg@users.noreply.github.com>
2023-02-16 09:27:45 -0300

    Revert "Revert "Feature/home screen""

--- DailyDoodle/ContentView.swift
+++ DailyDoodle/ContentView.swift
@@ -9,13 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color("Background").ignoresSafeArea()
+            VStack {
+                Elements()
+                Text("Desafio de Hoje")
+                    .frame(maxWidth: 330, alignment: .leading)
+                // .offset(x: -50)
+                    .font(Font.custom("Comfortaa-Bold", size: 28))
+                    .multilineTextAlignment(.leading)
+                Button {
+                    print("aiii papai cliquei")
+
+                } label: {
+                    Image("caxorro")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 330, height: 200)
+                        .cornerRadius(15)
+                        .clipShape(Rectangle())
+                        .overlay(
+                            Rectangle()
+                                .fill(.white).opacity(0.8)
+                                .frame(width: 330, height: 50)
+                                .cornerRadius(15)
+                                .padding(.top, 150)
+                                .overlay(alignment: .bottomTrailing) {
+                                    HStack {
+                                        Text("Iniciar desafio")
+                                            .font(Font.custom("Comfortaa-Bold", size: 24))
+                                            .foregroundColor(.black)
+
+                                        Image(systemName: "chevron.right.circle")
+                                            .foregroundColor(.black)
+                                            .font(.title)
+                                    } .padding(.trailing, 10)
+                                        .padding(.bottom, 5)
+                                }
+                        )
+                }
+                Text("Galeria")
+                    .frame(maxWidth: 330, alignment: .leading)
+                // .offset(x: -110)
+                    .font(Font.custom("Comfortaa-Bold", size: 28))
+                    .multilineTextAlignment(.leading)
+                GalleryPreviews().frame(maxWidth: 330, alignment: .leading)
+            }
         }
-        .padding()
     }
 }
 
